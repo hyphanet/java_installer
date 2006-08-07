04af1c500221ab0772d1eef5a602db04fb3c6c83
Florent Daignière <nextgens@freenetproject.org>
2006-08-07 13:35:57 +0000

    installer: detect if the proxy|fcp|console ports are bindable or not, workaround if not
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/new_installer@9944 67a373e5-eb02-0410-a15c-ee090a768436

--- res/unix/bin/1run.sh
+++ res/unix/bin/1run.sh
@@ -4,10 +4,12 @@ DST="$INSTALL_PATH"
 
 echo "install: $INSTALL_PATH"
 cd "$DST"
+
+# Tweak freenet.ini before the first startup
 if [[ -e stun ]]
 then
 	echo "Enabling the STUN plugin"
-	echo "pluginmanager.loadplugin=*@file://$INSTALL_PATH/plugins/JSTUN.jar;" >> freenet.ini
+	echo "pluginmanager.loadplugin=plugins.JSTUN@file://$INSTALL_PATH/plugins/JSTUN.jar;" >> freenet.ini
 	rm -f stun
 fi
 if [[ -e update ]]
@@ -17,6 +19,43 @@ then
 	rm -f update
 fi
 
+# Try to auto-detect the first aviable port for fproxy
+FPROXY_PORT=8888
+java -jar bin/bindtest.jar $FPROXY_PORT
+if [[ $? -ne 0 ]]
+then
+	FPROXY_PORT=8889
+	echo "Can not bind fproxy to 8888: let's try $FPROXY_PORT insteed."
+	java -jar bin/bindtest.jar $FPROXY_PORT
+	if [[ $? -ne 0 ]]
+	then
+		FPROXY_PORT=9999
+		echo "Can not bind fproxy to 8889: force it to $FPROXY_PORT insteed. You might have to edit freenet.ini by hand yourself to choose an aviable, bindable tcp port."
+	fi
+fi
+echo -e "fproxy.enabled=true\nfproxy.port=$FPROXY_PORT" >> freenet.ini
+
+# Try to auto-detect the first aviable port for fcp
+FCP_PORT=9481
+java -jar bin/bindtest.jar $FCP_PORT
+if [[ $? -ne 0 ]]
+then
+	FCP_PORT=9482
+	echo "Can not bind fcp to 9481: force it to $FCP_PORT insteed. You might have to edit freenet.ini by hand yourself to choose an aviable, bindable tcp port."
+fi
+echo -e "fcp.enabled=true\nfcp.port=$FCP_PORT" >> freenet.ini
+
+# Try to auto-detect the first aviable port for console
+CONSOLE_PORT=2323
+java -jar bin/bindtest.jar $CONSOLE_PORT
+if [[ $? -ne 0 ]]
+then
+	CONSOLE_PORT=2324
+	echo "Can not bind console to 2323: force it to $CONSOLE_PORT insteed. You might have to edit freenet.ini by hand yourself to choose an aviable, bindable tcp port."
+fi
+echo -e "console.enabled=true\nconsole.port=$CONSOLE_PORT" >> freenet.ini
+
+# We need the exec flag on /bin
 chmod a+rx -R $INSTALL_PATH/bin $INSTALL_PATH/lib
 
 echo "Downloading freenet-stable-latest.jar"
@@ -26,12 +65,14 @@ java -jar bin/sha1test.jar freenet-ext.jar "$DST" || exit 1
 echo "Downloading update.sh"
 java -jar bin/sha1test.jar update/update.sh "$DST" || exit 1
 chmod +x $DST/update.sh
+
+# Starting the node up
 ./run.sh start
 echo "Waiting for Freenet to startup"
 sleep 10
 echo "Starting up a browser"
-java -cp bin/browser.jar BareBonesBrowserLaunch "http://127.0.0.1:8888/"
-java -cp bin/browser.jar BareBonesBrowserLaunch "http://$INSTALL_PATH/welcome.html"
+java -cp bin/browser.jar BareBonesBrowserLaunch "http://127.0.0.1:$FPROXY_PORT/"
+java -cp bin/browser.jar BareBonesBrowserLaunch "file:///$INSTALL_PATH/welcome.html"
 
 echo "Finished"
 
--- res/windows/bin/1run.cmd
+++ res/windows/bin/1run.cmd
@@ -1,9 +1,33 @@
 @set PATH=%SYSTEMROOT%\System32\;%PATH%
 @cd "$INSTALL_PATH"
+
+:: Tweak freenet.ini
 @if exist stun echo pluginmanager.loadplugin=plugins.JSTUN@file:///$INSTALL_PATH\plugins\JSTUN.jar >> freenet.ini
 @del /F stun > NUL
 @if exist update echo node.updater.autoupdate=true >> freenet.ini
 @del /F update > NUL
+
+:: Try to detect a free, aviable port for fproxy
+@set FPROXY_PORT=8888
+@java -jar bin\bindtest.java %FPROXY_PORT% 
+@if not errorlevel 0 set FPROXY_PORT=8889
+@echo fproxy.enable=true >>freenet.ini
+@echo fproxy.port=%FPROXY_PORT% >>freenet.ini
+
+:: Try to detect a free, aviable port for fcp
+@set FCP_PORT=9481
+@java -jar bin\bindtest.java %FCP_PORT% 
+@if not errorlevel 0 set FCP_PORT=9482
+@echo fcp.enable=true >>freenet.ini
+@echo fcp.port=%FCP_PORT% >>freenet.ini
+
+:: Try to detect a free, aviable port for console
+@set CONSOLE_PORT=2323
+@java -jar bin\bindtest.java %CONSOLE_PORT% 
+@if not errorlevel 0 set CONSOLE_PORT=2324
+@echo console.enable=true >>freenet.ini
+@echo console.port=%CONSOLE_PORT% >>freenet.ini
+
 @echo "Downloading freenet-stable-latest.jar"
 @java -jar bin\sha1test.jar freenet-stable-latest.jar "$INSTALL_PATH"
 @echo "Downloading freenet-ext.jar"
@@ -13,10 +37,13 @@
 @echo "Installing the wrapper"
 @echo "Registering Freenet as a system service"
 @bin\wrapper-windows-x86-32.exe -i ../wrapper.conf
+
+:: Start the node up
 @net start freenet-darknet
 @echo "Waiting for freenet to startup"
 @ping -n 5 127.0.0.1 >nul
+
 @echo "Spawing up a browser"
-@start http://127.0.0.1:8888
+@start http://127.0.0.1:%FPROXY_PORT%/
 @start file:///$INSTALL_PATH/welcome.html
 @echo "Finished"

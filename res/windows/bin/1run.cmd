596680a52cee203d65663e6e43392a47155fb720
Florent Daignière <nextgens@freenetproject.org>
2006-08-13 15:59:41 +0000

    new_installer: register .fref files on windows
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/new_installer@10061 67a373e5-eb02-0410-a15c-ee090a768436

--- res/windows/bin/1run.cmd
+++ res/windows/bin/1run.cmd
@@ -1,10 +1,12 @@
 @set PATH=%SYSTEMROOT%\System32\;%PATH%
-@set INSTALL_PATH=C:\Program Files\Freenet
-@set JAVA_HOME=C:\Program Files\Java\jre1.5.0_07
+@set INSTALL_PATH=$INSTALL_PATH
+@set JAVA_HOME=$JAVA_HOME
 @cd %INSTALL_PATH%
 
-@echo "Registering FREF file extention"
+@echo "Registering .fref file extention"
 @echo Windows Registry Editor Version 5.00 >> fref.reg
+@echo [HKEY_CLASSES_ROOT\.fref] >> fref.reg
+@echo @="fref_auto_file" >> fref.reg
 @echo [HKEY_CLASSES_ROOT\fref_auto_file] >> fref.reg
 @echo @="Freenet node reference" >> fref.reg
 @echo "EditFlags"=dword:00000000 >> fref.reg
@@ -12,12 +14,14 @@
 @echo [HKEY_CLASSES_ROOT\fref_auto_file\DefaultIcon] >> fref.reg
 @echo @="shell32.dll,56" >> fref.reg
 @echo [HKEY_CLASSES_ROOT\fref_auto_file\shell] >> fref.reg
-@echo @="Connect" >> fref.reg
-@echo [HKEY_CLASSES_ROOT\fref_auto_file\shell\Connect] >> fref.reg
-@echo [HKEY_CLASSES_ROOT\fref_auto_file\shell\Connect\command] >> fref.reg
-@echo @="\"%JAVA_HOME%\\bin\\java.exe\"  \"-cp\"  \"%INSTALL_PATH%\\freenet.jar\" \"freenet.support.AddRef\" \"%1\"" >> fref.reg
-@regedit /s fref.reg
-:: @del /F fref.reg
+@echo @="Open" >> fref.reg
+:: We need to double escape it ... it doesn't work so let's try something else :p ... FTYPE
+:: @echo [HKEY_CLASSES_ROOT\fref_auto_file\shell\Connect] >> fref.reg
+:: @echo [HKEY_CLASSES_ROOT\fref_auto_file\shell\Connect\command] >> fref.reg
+:: @echo @="\"%JAVA%\\bin\\java.exe\"  \"-cp\"  \"%INST%\\freenet.jar\" \"freenet.support.AddRef\" \"%%1\"" >> fref.reg
+@regedit /s fref.reg > NUL
+@FTYPE fref_auto_file="$JAVA_HOME\bin\javaw.exe" -cp "$INSTALL_PATH\freenet.jar" freenet.support.AddRef "%%1" > NUL
+@del /F fref.reg
 
 @echo "Setting up plugins"
 :: Tweak freenet.ini

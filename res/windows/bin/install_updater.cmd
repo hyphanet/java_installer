e2f23afeda04c06fe9f9c3b1f92f2abb916b7064
Florent Daignière <nextgens@freenetproject.org>
2007-04-08 16:25:11 +0000

    new_installer: fix a bug when you don't select auto-update
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/new_installer@12555 67a373e5-eb02-0410-a15c-ee090a768436

--- res/windows/bin/install_updater.cmd
+++ res/windows/bin/install_updater.cmd
@@ -6,5 +6,8 @@
 @echo "Downloading update.cmd"
 @java -jar bin\sha1test.jar update/update.cmd . > NUL
 @echo node.updater.enabled=true >> freenet.ini
-@if exist update echo node.updater.autoupdate=true >> freenet.ini
+
+@if not exist update goto noautoupdate
+@echo node.updater.autoupdate=true >> freenet.ini
 @del /F update > NUL
+:noautoupdate

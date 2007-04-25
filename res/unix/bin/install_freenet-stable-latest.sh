b25da171eff260efbf3196db67b41df7206dbd15
Florent Daignière <nextgens@freenetproject.org>
2007-04-25 17:18:08 +0000

    new_installer: silent ajgenius's error
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/new_installer@12944 67a373e5-eb02-0410-a15c-ee090a768436

--- res/unix/bin/install_freenet-stable-latest.sh
+++ res/unix/bin/install_freenet-stable-latest.sh
@@ -6,4 +6,5 @@ cd "$INSTALL_PATH"
 
 echo "Downloading freenet-stable-latest.jar"
 java -jar bin/sha1test.jar freenet-stable-latest.jar "$INSTALL_PATH" &>/dev/null || exit 1 
-ln -s freenet-stable-latest.jar freenet.jar
+rm -f freenet.jar
+ln -sf freenet-stable-latest.jar freenet.jar 2>&1 >/dev/null

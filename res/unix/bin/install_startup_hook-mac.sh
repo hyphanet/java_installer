8a1287061cc25e63a79968911011664249953207
Florent Daignière <nextgens@freenetproject.org>
2007-04-27 21:28:59 +0000

    new_installer: make the node start on startup if installed on macosx < 10.3
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/new_installer@13019 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ res/unix/bin/install_startup_hook-mac.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+INSTALL_PATH="${INSTALL_PATH:-$PWD}"
+STARTUP_PATH="/Library/StartupItems/"
+SCRIPT="$STARTUP_PATH/Freenet/Freenet.sh"
+SCRIPT_PLIST="$STARTUP_PATH/Freenet/Freenet.plist"
+
+cd "$INSTALL_PATH"
+
+echo "Creating a startup script for Freenet"
+
+if test ! -d $STARTUP_PATH
+then
+	mkdir $STARTUP_PATH 2>&1 >/dev/null
+fi
+
+mkdir $STARTUP_PATH/Freenet 2>&1 >/dev/null
+rm -f $SCRIPT
+echo "#!/bin/sh" >> $SCRIPT
+echo ". /etc/rc.common" >> $SCRIPT
+echo "# This script will start up Freenet" >> $SCRIPT
+echo 'ConsoleMessage "Starting Freenet' >> $SCRIPT
+echo "export HOME=\"$INSTALL_PATH\"" >> $SCRIPT
+echo "cd \"$INSTALL_PATH\"" >> $SCRIPT
+echo "./run.sh start" >> $SCRIPT
+
+chmod 555 $SCRIPT
+
+rm -f $SCRIPT_PLIST
+echo '{' >>  $SCRIPT_PLIST
+echo "Description = \"Freenet\";" >>  $SCRIPT_PLIST
+echo "Provides = (\"Freenet\");" >>  $SCRIPT_PLIST
+echo "OrderPreference = \"None\";" >>  $SCRIPT_PLIST
+echo '}' >>  $SCRIPT_PLIST

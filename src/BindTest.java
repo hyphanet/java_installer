9a6e2b00572770809c75fdd9393b9d2ad1166c26
Florent Daignière <nextgens@freenetproject.org>
2007-05-01 15:46:09 +0000

    new_installer: the fix for #1329 breaks on windows :< ... YetANewWorkaround, part 3
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/new_installer@13082 67a373e5-eb02-0410-a15c-ee090a768436

--- src/BindTest.java
+++ src/BindTest.java
@@ -14,16 +14,13 @@ public class BindTest {
 			Integer port = Integer.valueOf(args[0]);
 			ServerSocket ss = null;
 			/* workaround a macos|windows problem */
-			String os = System.getProperty("os.name");
-			if(os != null && os.equalsIgnoreCase("Windows"))
-				ss = new ServerSocket(port.intValue());
-			else {
-				ss = new ServerSocket();
+			String os = System.getProperty("os.name").toLowerCase();
+			ss = new ServerSocket();
+			if(os.startsWith("mac"))
 				ss.setReuseAddress(false);
-				ss.bind(new InetSocketAddress("127.0.0.1:", port.intValue()));
-				if(!ss.isBound())
-					System.exit(1);
-			}
+			ss.bind(new InetSocketAddress("127.0.0.1:", port.intValue()));
+			if(!ss.isBound())
+				System.exit(1);
 
 			ss.setSoTimeout(200);
 			ss.accept();

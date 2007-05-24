9f2af5e1bfca1a73c12532ea110ca2faeb0df5c9
Florent Daignière <nextgens@freenetproject.org>
2007-05-24 22:29:41 +0000

    new_installer: doh, I forgot one file
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/new_installer@13364 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ res/unix/bin/install_upnp.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+INSTALL_PATH="${INSTALL_PATH:-$PWD}"
+
+cd "$INSTALL_PATH"
+
+if test -e upnp -a -e plug
+then
+	echo "Enabling the UP&P plugin"
+	mkdir plugins &>/dev/null
+	PLUGINS=`cat plug`
+	echo "plugins.UPnP.UPnP@file://$INSTALL_PATH/plugins/UPnP.jar;$PLUGINS" > plug2
+	mv -f plug2 plug
+	if test ! -e offline
+	then
+		java -jar bin/sha1test.jar plugins/UPnP.jar.url plugins &>/dev/null
+	fi
+	mv plugins/UPnP.jar.url plugins/UPnP.jar
+	rm -f plugins/UPnP.jar.url
+	rm -f upnp
+fi

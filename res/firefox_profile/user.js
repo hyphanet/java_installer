98699f2102a3544ddeef5a856bf4b807c7b8aac6
Florent Daignière <nextgens@freenetproject.org>
2008-03-15 04:04:01 +0000

    new_installer: more tweaks to the firefox profile we bundle; worksforme but needs serious testing
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/new_installer@18540 67a373e5-eb02-0410-a15c-ee090a768436

--- res/firefox_profile/user.js
+++ res/firefox_profile/user.js
@@ -1,9 +1,12 @@
 user_pref("browser.startup.homepage", "http://127.0.0.1:8888");
+user_pref("app.update.enabled", false);
+user_pref("extensions.update.notifyUser", false);
 user_pref("javascript.enabled", false);
 user_pref("security.enable_java", false);
 // See 1124: Possible hint on how to not get USK's googled when miss-pasted
 user_pref("keyword.enabled", false);
 user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.search.update", false);
 // Obvious performance tweaks
 user_pref("network.http.max-connections", 200);
 user_pref("network.http.max-connections-per-server", 200);
@@ -22,3 +25,13 @@ user_pref("network.cookie.enableForCurrentSessionOnly", false);
 user_pref("browser.safebrowsing.enabled", false);
 // See 1941: Firefox GoBrowsing
 user_pref("browser.safebrowsing.remoteLookups", false);
+// We don't want to import anything
+user_pref("network.cookie.prefsMigrated", true);
+// We don't want the popup when we click submit
+user_pref("security.warn_entering_secure", false);
+// Select the skin
+user_pref("general.skins.selectedSkin", "BlackJapanMAX");
+// Chose the default charset
+user_pref("intl.charsetmenu.browser.cache", "UTF-8");
+// Prevent plugin installation from that profile
+user_pref("xpinstall.whitelist.add", "");

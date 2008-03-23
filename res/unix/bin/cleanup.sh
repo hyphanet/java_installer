cf13869be3050bfd99b92861391cfebdea7ce3ce
Florent Daignière <nextgens@freenetproject.org>
2008-03-23 14:23:15 +0000

    new_installer: update the cleanup script
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/new_installer@18729 67a373e5-eb02-0410-a15c-ee090a768436

--- res/unix/bin/cleanup.sh
+++ res/unix/bin/cleanup.sh
@@ -2,10 +2,9 @@
 
 . "$HOME/_install_toSource.sh"
 rm -f "$HOME/_install_toSource.sh"
-cd "$INSTALL_PATH"
+cd "$INSTALL_PATH/bin"
 
 # We keep application installers in case users want to perform updates
-rm -f 1run.sh cleanup.sh detect_port_availability.sh install_freenet-ext.sh install_freenet-stable-latest.sh install_librarian.sh install_mdns.sh install_plugins.sh install_stun.sh install_updater.sh install_frost.sh setup.sh install_startup_hook-mac.sh offline opennet.install
-
+rm -f 1run.sh cleanup.sh detect_port_availability.sh setup.sh offline install_*.sh
 
 echo "All done, please click Next"

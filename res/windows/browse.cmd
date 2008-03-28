40e0fac1a36a3271f0117d63f1fdf5b93d2b1c33
Florent Daignière <nextgens@freenetproject.org>
2008-03-28 04:17:24 +0000

    new_installer: fix the 'the installer changed my default profile' bug spawning up an other instance of the main profile in any case
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/new_installer@18802 67a373e5-eb02-0410-a15c-ee090a768436

--- res/windows/browse.cmd
+++ res/windows/browse.cmd
@@ -17,6 +17,7 @@
 @if not exist firefox.location goto detectff
 @set /P FIREFOX=<firefox.location
 @if not defined FIREFOX goto detectff
+@start "" /B %FIREFOX% -no-remote "about:blank"
 @start "" /B %FIREFOX% -no-remote -P freenet "%URL%"
 @goto realEnd
 
@@ -31,6 +32,7 @@
 
 :: creation of the profile
 @echo Creating a Firefox profile for freenet
+@start "" /B %FIREFOX% -no-remote "about:blank"
 @%FIREFOX% -no-remote -CreateProfile "freenet %INSTALL_PATH%\firefox_profile" > NUL
 @start "" /B %FIREFOX% -no-remote -P freenet "%URL%"
 @goto end

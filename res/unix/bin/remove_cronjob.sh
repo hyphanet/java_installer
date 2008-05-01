51301183305a341b1e82a4f0d1516623f33f2189
Matthew Toseland <toad@amphibian.dyndns.org>
2008-05-01 15:06:24 +0000

    Fix remove_cronjob.sh.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/new_installer@19649 67a373e5-eb02-0410-a15c-ee090a768436

--- res/unix/bin/remove_cronjob.sh
+++ res/unix/bin/remove_cronjob.sh
@@ -4,7 +4,7 @@ crontab -l > crontab.tmp
 if grep -F crontab.tmp "@reboot   \"$INSTALL_PATH/run.sh\" start"
 then
 	echo Found service in crontab, removing it...
-	cat crontab.tmp | grep -v -F "@reboot   \"$INSTALL_PATH/run.sh\" start" > crontab.tmp.new
+	cat crontab.tmp | grep -v -F "@reboot   \"$INSTALL_PATH/run.sh\" start" - > crontab.tmp.new
 	crontab crontab.tmp.new
 fi
 rm -f crontab.tmp crontab.tmp.new

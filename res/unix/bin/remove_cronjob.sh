5e5c496775301083880f18308149eabfc472c8d7
Matthew Toseland <toad@amphibian.dyndns.org>
2008-05-01 14:21:08 +0000

    Hopefully this will remove the cronjob on uninstall through the uninstaller jar.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/new_installer@19645 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ res/unix/bin/remove_cronjob.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+crontab -l > crontab.tmp
+if grep -F crontab.tmp "@reboot   \"$INSTALL_PATH/run.sh\" start"
+then
+	echo Found service in crontab, removing it...
+	cat crontab.tmp | grep -v -F "@reboot   \"$INSTALL_PATH/run.sh\" start" > crontab.tmp.new
+	crontab crontab.tmp.new
+fi
+rm -f crontab.tmp crontab.tmp.new

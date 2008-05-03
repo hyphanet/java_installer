03a467920ea6c93200f198dd0f17e97b971e0854
Florent Daignière <nextgens@freenetproject.org>
2008-05-03 15:01:10 +0000

    new_installer: fix a bug I've just introduced
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/new_installer@19703 67a373e5-eb02-0410-a15c-ee090a768436

--- res/unix/bin/remove_cronjob.sh
+++ res/unix/bin/remove_cronjob.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if grep -c " #FREENET AUTOSTART - 8888$" -gt 0
+if test grep -c " #FREENET AUTOSTART - 8888$" -gt 0
 then
 	echo Found service in crontab, removing it...
 	crontab -l | grep -v " #FREENET AUTOSTART - 8888$" | crontab -

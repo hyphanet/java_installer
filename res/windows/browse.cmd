a4f1c494063d3fa7429f453ccf8d00c9dadf2dd5
Matthew Toseland <toad@amphibian.dyndns.org>
2008-05-14 17:03:24 +0000

    If the start fails, load iexplore directly.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/new_installer@19931 67a373e5-eb02-0410-a15c-ee090a768436

--- res/windows/browse.cmd
+++ res/windows/browse.cmd
@@ -58,6 +58,11 @@
 @del /f firefox.location
 @echo Trying to open "%URL%"
 @start "" "%URL%"
+@if errorlevel 1 goto argh
+@goto end
+:argh
+@echo Starting the page failed, attempting to load directly in IE
+@start "" /B "%ProgramFiles%\Internet Explorer\iexplore.exe" "%URL%"
 :end
 @del /f firefox.reg
 :realEnd

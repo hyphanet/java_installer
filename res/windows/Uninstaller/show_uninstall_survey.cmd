8e643396c59c36d68502c17fc1aaf87ee42e8929
Florent Daignière <nextgens@freenetproject.org>
2008-08-22 23:51:05 +0000

    new_installer: maybe fix the uninstall survey
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/new_installer@22111 67a373e5-eb02-0410-a15c-ee090a768436

--- res/windows/Uninstaller/show_uninstall_survey.cmd
+++ res/windows/Uninstaller/show_uninstall_survey.cmd
@@ -1,1 +1,10 @@
-@rundll32.exe shdocvw.dll,OpenURL http://spreadsheets.google.com/viewform?key=pARgKQ0i0ggo42b-G3If4Iw
+@set URL=http://spreadsheets.google.com/viewform?key=pARgKQ0i0ggo42b-G3If4Iw
+
+@echo Trying to open "%URL%"
+@start "" "%URL%"
+@if errorlevel 1 goto argh
+@goto end
+:argh
+@echo Starting the page failed, attempting to load directly in IE
+@start "" /B "%ProgramFiles%\Internet Explorer\iexplore.exe" "%URL%"
+:end

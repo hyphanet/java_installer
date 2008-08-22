62f60ea6ab90daf308d99974225eb1b3652dc97b
Florent Daignière <nextgens@freenetproject.org>
2008-08-22 18:37:43 +0000

    new_installer: ask the uninstalling users to fill in a survey for us
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/new_installer@22103 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ res/unix/Uninstaller/show_uninstall_survey.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd "$INSTALL_PATH"
+
+java -jar bin/browser.jar "http://spreadsheets.google.com/viewform?key=pARgKQ0i0ggo42b-G3If4Iw"
---
+++ res/windows/Uninstaller/show_uninstall_survey.cmd
@@ -0,0 +1,1 @@
+@rundll32.exe shdocvw.dll,OpenURL http://spreadsheets.google.com/viewform?key=pARgKQ0i0ggo42b-G3If4Iw

466930d574d35195e3b9cddd6b2413f4bd5f6fe1
Florent Daignière <nextgens@freenetproject.org>
2008-09-28 12:33:35 +0000

    firefox_profile: more tweaking
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/new_installer@22877 67a373e5-eb02-0410-a15c-ee090a768436

--- res/firefox_profile/user.js
+++ res/firefox_profile/user.js
@@ -2,6 +2,8 @@ user_pref("browser.shell.checkDefaultBrowser", false);
 user_pref("browser.startup.page", 1);
 user_pref("browser.startup.homepage", "http://127.0.0.1:8888");
 user_pref("app.update.enabled", false);
+user_pref("extensions.update.enabled", false);
+user_pref("browser.search.update", false);
 user_pref("config.lockdown.disable_extensions", true);
 user_pref("extensions.update.notifyUser", false);
 user_pref("javascript.enabled", false);
@@ -29,6 +31,8 @@ user_pref("network.cookie.cookieBehavior", 1);
 user_pref("network.cookie.lifetimePolicy", 2);
 user_pref("network.cookie.alwaysAcceptSessionCookies", false);
 user_pref("network.cookie.enableForCurrentSessionOnly", false);
+// Don't remember passwords
+user_pref("signon.rememberSignons", false);
 // Disable safebrowsing
 user_pref("browser.safebrowsing.enabled", false);
 // See 1941: Firefox GoBrowsing
@@ -47,3 +51,15 @@ user_pref("xpinstall.enabled", false);
 user_pref("xpinstall.whitelist.add", "");
 user_pref("plugin.scan.4xPluginFolder", false);
 user_pref("plugin.scan.SunJRE", false);
+// Disable caching
+user_pref("browser.cache.disk.capacity", 0);
+// Use the private-data-cleaning stuff
+user_pref("privacy.sanitize.sanitizeOnShutdown", true);
+user_pref("privacy.sanitize.promptOnSanitize", false);
+user_pref("privacy.item.history", true);
+user_pref("privacy.item.downloads", true);
+user_pref("privacy.item.formdata", true);
+user_pref("privacy.item.cache", true);
+user_pref("privacy.item.cookies", true);
+user_pref("privacy.item.passwords", true);
+user_pref("privacy.item.sessions", true);

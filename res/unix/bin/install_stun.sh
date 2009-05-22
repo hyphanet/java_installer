e02920420b7af6adf5b51636735f280d04c49845
Matthew Toseland <toad@amphibian.dyndns.org>
2009-05-22 16:21:27 +0100

    Remove unnecessary logging

--- res/unix/bin/install_stun.sh
+++ res/unix/bin/install_stun.sh
@@ -7,6 +7,5 @@ if test ! -d plugins; then mkdir plugins; fi 2>&1 >/dev/null
 	echo "Downloading the STUN plugin"
 	if test ! -f offline
 	then
-		echo java $JOPTS -jar bin/sha1test.jar JSTUN.jar plugins "$CAFILE"
 		java $JOPTS -jar bin/sha1test.jar JSTUN.jar plugins "$CAFILE" >/dev/null 2>&1
 	fi

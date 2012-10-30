b698587a6ee01255b7f50a6354c24de29d3614a0
Matthew Toseland <toad@amphibian.dyndns.org>
2012-10-30 12:45:51 +0000

    Add script for Windows online install too. We should probably get rid of both Windows support and online install support though...

---
+++ res/windows/bin/install_bcprov.cmd
@@ -0,0 +1,11 @@
+@set PATH=%SYSTEMROOT%\System32\;%PATH%
+@set INSTALL_PATH=$INSTALL_PATH
+@set JAVA_HOME=$JAVA_HOME
+@set CAFILE=startssl.pem
+@cd /D %INSTALL_PATH%
+@if exist .isInstalled goto end
+
+@if exist offline goto end
+@echo Downloading bcprov-jdk15on-147.jar
+@java -jar bin\sha1test.jar bcprov-jdk15on-147.jar . %CAFILE% > NUL
+:end

d465c868e2367ee3a8856d145c716b69e3c78fa7
Matthew Toseland <toad@amphibian.dyndns.org>
2012-10-30 12:38:30 +0000

    Download bcprov for the online installer too.

---
+++ res/unix/bin/install_bcprov.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+. "$HOME/_install_toSource.sh" || exit 0
+cd "$INSTALL_PATH"
+
+if test ! -f offline
+then
+	echo "Downloading bcprov-jdk15on-147.jar"
+	java $JOPTS -jar bin/sha1test.jar bcprov-jdk15on-147.jar "$INSTALL_PATH" "$CAFILE" >/dev/null 2>&1 || exit 1
+fi

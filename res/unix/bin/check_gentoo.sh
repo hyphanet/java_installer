7c04f37b934696cd10db64df7ee046107facfb4d
Florent Daigniere <nextgens@freenetproject.org>
2014-08-24 16:06:14 +0200

    fix bug #6217 properly: workaround setups where 'java' is a shell script

--- res/unix/bin/check_gentoo.sh
+++ res/unix/bin/check_gentoo.sh
@@ -1,9 +1,20 @@
-#!/bin/bash
+#!/bin/sh
+
 . "$HOME/_install_toSource.sh" || exit 0
 cd "$INSTALL_PATH"
-if test ! -f /etc/os-release; then exit; fi
-source /etc/os-release
-if test "$NAME" = "Gentoo"; then
-	echo Working around wrapper bug on Gentoo...
-	sed -i "s/^wrapper.java.command=.*$/wrapper.java.command=\/etc\/java-config-2\/current-system-vm\/bin\/java/" wrapper.conf
-fi
+
+# The wrapper needs an ELF binary... some distros are using a shell wrapper
+# see bug #6217
+CANDIDATES="$JAVA_HOME/bin/java `which java` /etc/java-config-2/current-system-vm/bin/java /usr/lib/jvm/java-default-runtime/bin/java /usr/lib/jvm/java-7-openjdk/jre/bin/java"
+for candidate in $CANDIDATES
+do
+	if head -1 "$candidate"|grep '^#' >/dev/null 2>&1
+	then
+		echo "Your java executable at $candidate is a script... looking for alternatives..."
+	else
+		echo "Your java executable at $candidate seems suitable"
+		ESCAPED_CANDIDATE=`echo "$candidate"|sed 's/\(\/\)/\\\\\1/g'`
+		sed -i "s/^wrapper.java.command=.*$/wrapper.java.command=$ESCAPED_CANDIDATE/" wrapper.conf
+		break
+	fi
+done

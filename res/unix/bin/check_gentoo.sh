1417c32c88477ca7dc199d5104c4ec65ce89ab00
Florent Daigniere <nextgens@freenetproject.org>
2014-08-24 17:24:27 +0200

    operhiem1 is right; we need an existence check too

--- res/unix/bin/check_gentoo.sh
+++ res/unix/bin/check_gentoo.sh
@@ -8,13 +8,16 @@ cd "$INSTALL_PATH"
 CANDIDATES="$JAVA_HOME/bin/java `which java` /etc/java-config-2/current-system-vm/bin/java /usr/lib/jvm/java-default-runtime/bin/java /usr/lib/jvm/java-7-openjdk/jre/bin/java"
 for candidate in $CANDIDATES
 do
-	if head -1 "$candidate"|grep '^#' >/dev/null 2>&1
+	if test -s "$candidate"
 	then
-		echo "Your java executable at $candidate is a script... looking for alternatives..."
-	else
-		echo "Your java executable at $candidate seems suitable"
-		ESCAPED_CANDIDATE=`echo "$candidate"|sed 's/\(\/\)/\\\\\1/g'`
-		sed -i "s/^wrapper.java.command=.*$/wrapper.java.command=$ESCAPED_CANDIDATE/" wrapper.conf
-		break
+		if head -1 "$candidate"|grep '^#' >/dev/null 2>&1
+		then
+			echo "Your java executable at $candidate is a script... looking for alternatives..."
+		else
+			echo "Your java executable at $candidate seems suitable"
+			ESCAPED_CANDIDATE=`echo "$candidate"|sed 's/\(\/\)/\\\\\1/g'`
+			sed -i "s/^wrapper.java.command=.*$/wrapper.java.command=$ESCAPED_CANDIDATE/" wrapper.conf
+			break
+		fi
 	fi
 done

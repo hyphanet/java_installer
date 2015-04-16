9f5ea5115a6e797f262df607dbf4d4603f653059
Stephen Oliver <steve@infincia.com>
2015-04-16 15:43:15 -0400

    Mac Tray: set FNFCPWrapper to GPLv2+ to match the rest of the codebase for now

--- res/mac/FreenetTray/src/FNFCPWrapper.m
+++ res/mac/FreenetTray/src/FNFCPWrapper.m
@@ -1,7 +1,8 @@
 /* 
     Copyright (C) 2015 Stephen Oliver <steve@infincia.com>
     
-    This code is distributed under the GNU General Public License, version 2.
+    This code is distributed under the GNU General Public License, version 2 
+    (or at your option any later version).
     
     3rd party libraries may be distributed under an alternate Open Source license.
     
